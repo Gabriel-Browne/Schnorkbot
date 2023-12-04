079dbd1356fed9de9759703d10ff8e130859baa6
daxelb <damianabrowne@gmail.com>
2023-12-04 17:24:16 -0500

    Added meeting notes.

---
+++ index.js
@@ -0,0 +1,16 @@
+// Import necessary modules
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
