30f0bf8eb98d713978e4e6647e71fa8170a576b3
daxelb <damianabrowne@gmail.com>
2024-01-17 21:35:06 -0500

    fixed color again

--- src/entities/Zombie.js
+++ src/entities/Zombie.js
@@ -1,15 +1,17 @@
+// this entity definition is basically a copy-paste of the Bot entity definition, so refer to that for comments.
+
 import * as PIXI from 'pixi.js';
-import Position from '../components/Position';
-import Velocity from '../components/Velocity';
-// const INIT_ZOMBIE_SPEED
+import Position from '../components/Position.js';
+import Velocity from '../components/Velocity.js';
 
 class Zombie {
   INIT_SPEED = 1;
 
   constructor(x, y) {
     this.position = new Position(x, y);
-    this.velocity = new Velocity(Zombie.INIT_ZOMBIE_SPEED);
+    this.velocity = new Velocity(this.INIT_SPEED);
     this.sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+    this.sprite.tint = 0xff0000;
     this.sprite.anchor.set(0.5);
     this.sprite.position.set(x, y);
   }
