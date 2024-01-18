d51546f844ec4fd404350193cd6d1a10702e4e82
daxelb <damianabrowne@gmail.com>
2024-01-17 19:22:00 -0500

    added more stuff to EntityManager

--- src/utils/EntityManager.js
+++ src/utils/EntityManager.js
@@ -1,10 +1,45 @@
+import Zombie from '../entities/Zombie.js';
+import Bot from '../entities/Bot.js';
+
+// this class defines an object that essentially is just a list of entities,
+// the class provides some methods that may be useful when dealing with entities
 class EntityManager {
   constructor() {
     this.entities = [];
+    this.entitiesByType = {};
   }
 
   addEntity(entity) {
     this.entities.push(entity);
+    if (this.entitiesByType[entity.constructor.name]) {
+      this.entitiesByType[entity.constructor.name].push(entity);
+    } else {
+      this.entitiesByType[entity.constructor.name] = [entity];
+    }
+  }
+
+  removeEntity(entity) {
+    this.entities = this.entities.filter((e) => e !== entity);
+    this.entitiesByType[entity.constructor.name] = this.entitiesByType[
+      entity.constructor.name
+    ].filter((e) => e !== entity);
+  }
+
+  getEntitiesByType(type) {
+    return this.entitiesByType[type];
+  }
+
+  getBots() {
+    return this.getEntitiesByType(Bot.name);
+  }
+
+  getZombies() {
+    return this.getEntitiesByType(Zombie.name);
+  }
+
+  removeAllEntitiesOfType(type) {
+    this.entities = this.entities.filter((e) => !(e instanceof type));
+    this.entitiesByType[type.name] = [];
   }
 }
 
